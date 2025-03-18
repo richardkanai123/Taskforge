4ec3b5791b8e47e2ade2d3a5523a9d391d570127
Richard Kanai(李凯男) <richardkanainjeri@gmail.com>
2025-03-18 14:14:18 +0300

    temp ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-images: {
-deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-remotePatterns: [
-    {
-    protocol: 'https',
-    hostname: 'ui-avatars.com',
-    pathname: '**',
-    },
-    {
-    protocol: 'https',
-    hostname: 'randomuser.me',
-    pathname: '**',
-    },
-],
-}
+	/* config options here */
+	images: {
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "ui-avatars.com",
+				pathname: "**",
+			},
+			{
+				protocol: "https",
+				hostname: "randomuser.me",
+				pathname: "**",
+			},
+		],
+  },
+  
+  // disable type check on building
+  typescript: {
+   
+    ignoreBuildErrors: true
+ }
 };
 
 export default nextConfig;
